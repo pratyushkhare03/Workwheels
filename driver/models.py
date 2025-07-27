4ee530ac3024d13b6c526c04193312d6a6b21bae
ARPIT KUMAR <232010@kit.ac.in>
2025-07-28 01:27:28 +0530

    driver modules updated

--- driver/models.py
+++ driver/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class driver(models.Model):
+class Driver(models.Model):
     username=models.OneToOneField(User, on_delete=models.CASCADE)
     carModel=models.CharField(max_length=50)
     carNumberPlate=models.CharField(max_length=50)
